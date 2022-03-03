8d5db871b7723b19edabbbe329c8d78ab5bee3d0
narimanAbyar <nariman.abyar11@gmail.com>
2022-03-03 12:56:09 +0330

    compelete Formik

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import './App.css';
+import React from "react";
+import { Formik,Field,Form,ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+function App(){
+  
+  const SchemaLoginForm = Yup.object().shape({
+    username: Yup.string().required("Username is required."),
+    password: Yup.string()
+      .matches(
+        /^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$/,
+        "Password has wrong character."
+      )
+      .required("Password is required"),
+  });
+
+  return(
+    <Formik
+    validationSchema={SchemaLoginForm}
+    initialValues={{username:"",password:""}}
+    onSubmit={(values)=>{
+      alert("username"+values.username+"password"+values.password)
+    }}
+    >
+      {(()=>(
+        <Form>
+          <div>
+            <label htmlFor="username">username</label>
+            <Field type="username" name="username"/>
+            <ErrorMessage name="username">
+              {(msg) => <di>{msg}</di>}
+            </ErrorMessage>
+          </div>
+          <div>
+            <label htmlFor="password">password</label>
+            <Field type="password" name="password"/>
+            <ErrorMessage name="password">
+              {(msg) => <di>{msg}</di>}
+            </ErrorMessage>
+          </div>
+          <div>
+            <button type="submit">submit</button>
+          </div>
+        </Form>
+      )
+
+      )}
+    </Formik>
+  )
 }
 
 export default App;
